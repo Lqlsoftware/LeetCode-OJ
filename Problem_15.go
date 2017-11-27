4701be2e0c1c1eb3bc7ab9dfaf0706f144f88a9b
Lqlsoftware <lqlsoft@hotmail.com>
2017-11-27 14:09:56 +0800

    daily work

--- Problem_15.go
+++ Problem_15.go
@@ -1,8 +1,14 @@
 /*
-Roman to Integer
-Given a roman numeral, convert it to an integer.
-
-Input is guaranteed to be within the range from 1 to 3999.
+3Sum
+Given an array S of n integers, are there elements a, b, c in S such that a + b + c = 0?
+Find all unique triplets in the array which gives the sum of zero.
+Note: The solution set must not contain duplicate triplets.
+For example, given array S = [-1, 0, 1, 2, -1, -4],
+A solution set is:
+[
+  [-1, 0, 1],
+  [-1, -1, 2]
+]
 */
 
 package main
