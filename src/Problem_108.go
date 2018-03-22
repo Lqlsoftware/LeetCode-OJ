98729162bcea58f49aed9104a26a5793c596da69
Lqlsoftware <lqlsoft@hotmail.com>
2018-03-22 16:04:06 +0800

    solve problem 108

---
+++ src/Problem_108.go
@@ -0,0 +1,30 @@
+/*
+Convert Sorted Array to Binary Search Tree
+Given an array where elements are sorted in ascending order, convert it to a height balanced BST.
+For this problem, a height-balanced binary tree is defined as a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
+
+Example:
+Given the sorted array: [-10,-3,0,5,9],
+One possible answer is: [0,-3,9,-10,null,5], which represents the following height balanced BST:
+
+      0
+     / \
+   -3   9
+   /   /
+ -10  5
+*/
+
+package main
+
+// recursively
+func sortedArrayToBST(nums []int) *TreeNode {
+	if len(nums) < 1 {
+		return nil
+	}
+	root := len(nums) / 2
+	return &TreeNode{
+		Val: 	nums[root],
+		Left: 	sortedArrayToBST(nums[:root]),
+		Right: 	sortedArrayToBST(nums[root + 1:]),
+	}
+}
