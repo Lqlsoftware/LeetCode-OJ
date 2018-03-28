0d8782884af681886be68cf1648f3b653eab95f5
Lqlsoftware <lqlsoft@hotmail.com>
2018-03-28 19:44:35 +0800

    solve problem 117

---
+++ src/Problem_117.go
@@ -0,0 +1,51 @@
+/*
+Populating Next Right Pointers in Each Node II
+Follow up for problem "Populating Next Right Pointers in Each Node".
+What if the given tree could be any binary tree? Would your previous solution still work?
+
+Note:
+You may only use constant extra space.
+For example,
+Given the following binary tree,
+         1
+       /  \
+      2    3
+     / \    \
+    4   5    7
+After calling your function, the tree should look like:
+         1 -> NULL
+       /  \
+      2 -> 3 -> NULL
+     / \    \
+    4-> 5 -> 7 -> NULL
+*/
+
+package main
+
+type TreeLinkNode struct {
+	Val 	int
+	Left	*TreeLinkNode
+	Right	*TreeLinkNode
+	Next	*TreeLinkNode
+}
+
+// 在访问父母的时候将孩子连接起来 下一层从连接的孩子开始访问
+func connect(root *TreeLinkNode) {
+	curr := root
+	fakeHead := &TreeLinkNode{0,nil,nil,nil}
+	for curr != nil {
+		last := fakeHead
+		for curr != nil {
+			if curr.Left != nil {
+				last.Next = curr.Left
+				last = last.Next
+			}
+			if curr.Right != nil {
+				last.Next = curr.Right
+				last = last.Next
+			}
+			curr = curr.Next
+		}
+		last.Next,curr = nil,fakeHead.Next
+	}
+}
