e345e8a62fffcb5d073b7c081a285ac89610f3b7
RobinLu <robin@lqlsoftware.cn>
2021-01-31 23:00:43 +0800

    Problem 213. House Robber II

---
+++ src/Problem 213. House Robber II.cpp
@@ -0,0 +1,23 @@
+// 213. House Robber II
+
+// Max (Rob (0 ~ n - 1), Rob (1 ~ n))
+// So that the first and the last house will not be both robbed in optimal solution
+class Solution {
+private:
+    int robI(vector<int>& nums, int start, int end) {
+        int prev = 0, last = 0, temp;
+        for (int i = start;i < end;i++) {
+            temp = last;
+            last = max(last, prev + nums[i]);
+            prev = temp;
+        }
+        return last;
+    }
+public:
+    int rob(vector<int>& nums) {
+        int m = nums.size();
+        if      (m == 0) return 0;
+        else if (m == 1) return nums[0];
+        return max(robI(nums, 0, m - 1), robI(nums, 1, m));
+    }
+};
