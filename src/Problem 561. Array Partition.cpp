eb01ca6f4155b93f124703d181e2205e77640ddd
RobinLu <robin@lqlsoftware.cn>
2021-03-26 16:30:36 +0800

    Problem 561. Array Partition

---
+++ src/Problem 561. Array Partition.cpp
@@ -0,0 +1,15 @@
+// 561. Array Partition I
+
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int arrayPairSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int ret = 0;
+        for (int i = 0;i < nums.size();i += 2) ret += nums[i];
+        return ret;
+    }
+};
